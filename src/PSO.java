336ce16daa82393c6a01dbdd06afdc87723c04e1
Evgeny Sorokin <evgerher1l@gmail.com>
2017-11-05 20:29:40 +0300

    ChordSequence PSO created

--- src/PSO.java
+++ src/PSO.java
@@ -4,20 +4,34 @@ import java.util.Random;
  * Created by evger on 03-Nov-17.
  */
 public class PSO {
-    private static Random random;
-    {
+    private Random random;
+    double values[];
+    public double globalBest[] = {60, 60, 60};
+    public double globalFitness = 0;
+
+    PSO(double values[]) {
+        this.values = values;
         random = new Random();
     }
 
-    public static class ChordOptimization {
-        private static double values[];
-        private static double c1;
-        private static double c2;
-        private static double m;
-        private static int bestTonics[] = {60, 64, 65};
+    public Chord[] generateChords() {
+        ChordSequence chSeq = new ChordSequence(values);
+        ChordOptimization chOpt = new ChordOptimization();
+
+        ChordSequence.Particle bestChordBases = chSeq.generateBaseOfChords();
+
+        return new Chord[1];
+    }
+
+    public class ChordSequence {
+        private double values[];
+        private double c1;
+        private double c2;
+        private double m;
+        private int bestTonics[] = {60, 64, 65};
         final int population = 30;
 
-        ChordOptimization(double values[]) {
+        ChordSequence(double values[]) {
             this.values = values;
         }
 
@@ -26,9 +40,6 @@ public class PSO {
             public double myBest[];
             public double velocity[];
             public double fitness;
-
-            public final double globalBest[] = {60, 60, 60};
-            public static double globalFitness = 0;
 
             Particle(int notesAmount) {
                 notes = new double[notesAmount];
@@ -50,14 +61,17 @@ public class PSO {
             return particles;
         }
 
-        public Particle generateChords() {
+        public Particle generateBaseOfChords() {
             Particle particles[] = generateParticles();
 
             optimize(particles);
 
+            int index = 0;
+            for (int i = 1; i < particles.length; i++)
+                if (particles[i].fitness < particles[index].fitness)
+                    index = i;
 
-            Particle particle = new Particle(3);
-            return particle;
+            return particles[index];
         }
 
         private void optimize(Particle[] particles) {
@@ -65,23 +79,23 @@ public class PSO {
                 double fitness = fitnessFunction(p);
                 if (fitness < p.fitness) {
                     p.fitness = fitness;
-                    for (int i = 0; i < 3; i++)
+                    for (int i = 0; i < 16; i++)
                         p.myBest[i] = p.notes[i];
                 }
 
-                if (fitness < p.globalFitness) {
-                    p.globalFitness = fitness;
-                    for (int i = 0; i < 3; i++)
-                        p.globalBest[i] = p.notes[i];
+                if (fitness < globalFitness) {
+                    globalFitness = fitness;
+                    for (int i = 0; i < 16; i++)
+                        globalBest[i] = p.notes[i];
                 }
             }
 
             for (Particle p : particles) {
-                int rand1 = random.nextInt(1);
-                int rand2 = random.nextInt(1);
+                int rand1 = random.nextInt(2);
+                int rand2 = random.nextInt(2);
 
                 for (int i = 0; i < p.velocity.length; i++)
-                    p.velocity[i] = (int)(m * p.velocity[i] + c1 * rand1 * (p.myBest[i] - p.notes[i]) + c2 * rand2 * (p.globalBest[i] - p.notes[i]));
+                    p.velocity[i] = m * p.velocity[i] + c1 * rand1 * (p.myBest[i] - p.notes[i]) + c2 * rand2 * (globalBest[i] - p.notes[i]);
 
             }
         }
@@ -98,11 +112,11 @@ public class PSO {
 
     }
 
-    public static class BarOptimization {
+    public class ChordOptimization {
 
     }
 
-    public static class PairNoteOptimization {
+    public class PairNoteOptimization {
 
     }
 }
